d38525250133e8c0bde944c9e93e967cfb925783
nandini-kasu <nandini.kasu@vtfintech.com>
2020-08-18 23:05:58 +0530

    users api's modification

--- users/views.py
+++ users/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from .serializers import UserSerializer
+from .serializers import UserSerializer, UserDetailSerializer
 from .models import User
 from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated, AllowAny
@@ -9,39 +9,42 @@ from django.contrib.auth import authenticate,login,logout
 
 # Create your views here.
 class UserViewSet(viewsets.ModelViewSet):
-    serializer_class = UserSerializer
+    serializer_class = UserDetailSerializer
     permission_classes = [IsAuthenticated]
 
     def get_queryset(self):
         queryset = User.objects.filter(pk = self.request.user.id)
         return queryset
     
-    @action(detail=True, methods=['post'], permission_classes=[AllowAny])
+    @action(detail=False, methods=['post'], permission_classes=[AllowAny])
     def sign_up(self, request):
         data = request.data
-        user_serializer = serializers.UserSerializer(data=data)
+        user_serializer = UserSerializer(data=data)
         user_serializer.is_valid(raise_exception=True)
         user_serializer.save()
         return Response({'data':data})
     
-    @action(detail=True, methods=['post'], permission_classes=[AllowAny])
+    @action(detail=False, methods=['post'], permission_classes=[AllowAny])
     def login(self, request):
         data = request.data
-        username = data.get('username')
-        password = data.get('password')
+        username = data.get('username', None)
+        password = data.get('password', None)
+
         user = authenticate(username=username, password=password)
+
         if user is not None:
             if user.is_active:
                 login(request, user)
                 return Response({
                     "is_authenticated": True,
-                    "user": serializers.UserSerializer(request.user).data,
-                    "auth_token": user.auth_token.key
+                    "user": UserDetailSerializer(request.user).data
                 })
+            else:
+                return Response(status=status.HTTP_404_NOT_FOUND)
         else:
-            return Response({"is_authenticated": False,"message":"Email ID is Wrong"}, status=status.HTTP_403_FORBIDDEN)
+            return Response(status=status.HTTP_404_NOT_FOUND)
 
-    @action(detail=True, methods=['post'], permission_classes=[IsAuthenticated])
+    @action(detail=False, methods=['post'], permission_classes=[IsAuthenticated])
     def logout(self, request):
         logout(request)
         return Response({"logged_out": True})
